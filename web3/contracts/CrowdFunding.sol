ac7d336b64b5acfff5a6bd00d6d689cf0dd62c61
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-01 21:10:07 -0400

    Update CrowdFunding.sol
    -add donate section

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -16,13 +16,37 @@ contract CrowdFunding {
     }
 
        // so we can access
-    mapping(uint256 => Campaign) public campaign;
+    mapping(uint256 => Campaign) public campaigns;
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign() {}
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image ) public returns(uint256) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
 
-    function donateToCampaign() {}
+        //a check of the campaign
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.amountCollected = 0;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return numberOfCampaigns -1;
+    }
+
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+    }
 
     function getDonators() {}
 
