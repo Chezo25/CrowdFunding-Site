760e00170621de9f7aef343b1f144296e53c3918
Chezley Stoddard <47767271+Chezo25@users.noreply.github.com>
2023-01-02 05:28:23 +0000

    -big dump

--- client_side/src/index.js
+++ client_side/src/index.js
@@ -1,13 +1,20 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import {BrowserRouter as Router } from 'react-router-dom';
+import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+
 
 import App from './App';
-
+import * as serviceWorker from './serviceWorker';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+      <Route path="/" component={App} />
+      </Router>
+  </ThirdwebProvider>
+  
+ 
 );
