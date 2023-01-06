8515efd905cc724fa03525d0ffc4c60d52bc09ab
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-05 22:42:12 -0400

    files up

--- client_side/src/index.js
+++ client_side/src/index.js
@@ -1,20 +1,23 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import {BrowserRouter as Router } from 'react-router-dom';
-import {ChainId, ThirdwebProvider} from '@thirdweb-dev/react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
-root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-    <Router>
-      <Route path="/" component={App} />
-      </Router>
-  </ThirdwebProvider>
-  
- 
+root.render( <ThirdwebProvider desiredChainId = { ChainId.Goerli }>
+    <Router >
+    
+    <Route path = "/"
+    component = { App }
+    /> </Router>
+     </ThirdwebProvider>
+    
+    
+
 );
--- client_side/tailwind.config.js
+++ client_side/tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ["./src/**/*.{html,js}",
+        "./src/**/*.{ts,tsx}",
+        "./src/**/*.{vue,jsx}",
+    ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                epilogue: ['Epilogue', 'sans-serif'],
+            },
+            boxShadow: {
+                secondary: '10px 10px 20px rgba(2, 2, 2, 0.25)',
+            },
+        },
     },
     plugins: [],
 }
