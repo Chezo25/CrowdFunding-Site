b1871178dc0fc74b973e59d607abd1ac072ebd28
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-06 23:55:42 -0400

    Update SideBar.jsx
    -update

--- client_side/src/components/SideBar.jsx
+++ client_side/src/components/SideBar.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { logo, sun } from '../assets';
+import { navlinks } from '../constants';
 
 const SideBar = () => {
+  const navigate = useNavigate();
+  const [isActive, setIsActive] = useState('dashboard');
+  
   return (
     <div>SideBar</div>
   )
