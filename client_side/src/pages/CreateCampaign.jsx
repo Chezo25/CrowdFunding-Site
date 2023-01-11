298d706ff50467bf74f47332ee6fa382e96355dd
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-10 22:04:41 -0400

    Update CreateCampaign.jsx

--- client_side/src/pages/CreateCampaign.jsx
+++ client_side/src/pages/CreateCampaign.jsx
@@ -56,7 +56,7 @@ const CreateCampaign = () => {
             value={form.description}
             handleChange={(e) => handleFormFieldChange('description', e)}
           />
-
+            {/* banner under the form */}
         <div className="w-full flex justify-start items-center p-4 bg-[#8c6dfd] h-[120px] rounded-[10px]">
           <img src={money} alt="money" className="w-[40px] h-[40px] object-contain"/>
           <h4 className="font-epilogue font-bold text-[25px] text-white ml-[20px]">You will get 100% of the raised amount</h4>
