2bc395faee14ab7afd34eee32c6286b1b9bb3f9f
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-15 21:37:26 -0400

    Update Home.jsx

--- client_side/src/pages/Home.jsx
+++ client_side/src/pages/Home.jsx
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+
+import { useStateContext } from '../context'
 
 const Home = () => {
+  // this is use the fecth from the smart contract
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
   return (
     <div>Home</div>
   )
