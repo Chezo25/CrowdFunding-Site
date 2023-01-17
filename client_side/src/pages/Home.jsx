27420033f8809e3e3ca676d85ed2bd2b4a7dac78
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-16 23:06:36 -0400

    add display comp

--- client_side/src/pages/Home.jsx
+++ client_side/src/pages/Home.jsx
@@ -6,6 +6,23 @@ const Home = () => {
   // this is use the fecth from the smart contract
   const [isLoading, setIsLoading] = useState(false);
   const [campaigns, setCampaigns] = useState([]);
+
+  const { address, contract, getCampaigns } = useStateContext();
+
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
+
+  // you to check if the contract is there to make it whole
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
+
+
+
   return (
     <div>Home</div>
   )
