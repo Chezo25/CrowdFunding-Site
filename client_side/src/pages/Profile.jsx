1e3a6de053f4835577870e36469bceed85ef047f
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-19 23:39:38 -0400

    profile

---
+++ client_side/src/pages/Profile.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
+import { DisplayCampaigns } from '../components'
+import { useStateContext } from '../context'
+
+const Profile = () => {
+    // this is use the fecth from the smart contract
+    const [isLoading, setIsLoading] = useState(false);
+    const [campaigns, setCampaigns] = useState([]);
+
+    const { address, contract, getCampaigns } = useStateContext();
+
+    const fetchCampaigns = async() => {
+        setIsLoading(true);
+        const data = await getCampaigns();
+        setCampaigns(data);
+        setIsLoading(false);
+    }
+
+    // you to check if the contract is there to make it whole
+    useEffect(() => {
+        if (contract) fetchCampaigns();
+    }, [address, contract]);
+
+
+
+    return (
+        // shows the campaign cards
+        <
+        DisplayCampaigns title = "All Campaigns"
+        isLoading = { isLoading }
+        campaigns = { campaigns }
+        />
+    )
+}
+
+export default Profile
