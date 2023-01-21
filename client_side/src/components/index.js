b2afd17b1413a178d18c8187985d086dd3dcbcea
scrt squirrel_Stod_25 <chezling.stoddard@gmail.com>
2023-01-20 23:38:41 -0400

    awake
    cool

--- client_side/src/components/index.js
+++ client_side/src/components/index.js
@@ -10,7 +10,7 @@ export { default as DisplayCampaigns }
 from './DisplayCampaigns';
 export { default as FundCard }
 from './FundCard.jsx';
-// export { default as CountBox }
-// from './CountBox';
+export { default as CountBox }
+from './CountBox';
 export { default as Loader }
 from './Loader';
